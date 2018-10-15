9e022b30bc6fb2c6bcdaa91649b296f93c6a3c1f
thaerlabs <abbas.t249@gmail.com>
2018-10-16 00:05:43 +0400

    add base Vue app

---
+++ client/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Hello {{message}}</h1>
+</template>
+
+<script>
+import Vue from 'vue';
+
+export default {
+  data() {
+    return {
+      message: 'World'
+    };
+  }
+};
+</script>
+
